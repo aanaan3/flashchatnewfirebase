2e68189612d70742679b66b271cb58379a2faa77
aan <aanaan3@gmail.com>
2023-03-26 02:07:02 +0300

    First Commit

---
+++ app/src/main/java/com/aanapp/flashchatnewfirebase/ChatListAdapter.java
@@ -0,0 +1,133 @@
+package com.aanapp.flashchatnewfirebase;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
+public class ChatListAdapter extends BaseAdapter {
+
+    private Activity mActivity;
+    private DatabaseReference mDatabaseReference;
+    private String mDisplayName;
+    private ArrayList<DataSnapshot> mSnapshotList;
+
+    private ChildEventListener mListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            mSnapshotList.add(snapshot);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    };
+
+    public ChatListAdapter(Activity activity, DatabaseReference databaseReference, String displayName) {
+        mActivity = activity;
+        mDisplayName = displayName;
+
+        mDatabaseReference = databaseReference.child("messages");
+        mDatabaseReference.addChildEventListener(mListener);
+
+        mSnapshotList = new ArrayList<>();
+    }
+
+    static class ViewHolder{
+        TextView authorName;
+        TextView body;
+        LinearLayout.LayoutParams params;
+    }
+
+    @Override
+    public int getCount() {
+        return mSnapshotList.size();
+    }
+
+    @Override
+    public InstantMessage getItem(int i) {
+        DataSnapshot snapshot = mSnapshotList.get(i);
+        return snapshot.getValue(InstantMessage.class);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if (view == null){
+            LayoutInflater inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.chat_msg_row,viewGroup,false);
+            final ViewHolder holder = new ViewHolder();
+            holder.authorName = (TextView) view.findViewById(R.id.author);
+            holder.body = (TextView) view.findViewById(R.id.message);
+            holder.params = (LinearLayout.LayoutParams) holder.authorName.getLayoutParams();
+            view.setTag(holder);
+        }
+
+        final InstantMessage message = getItem(i);
+        final  ViewHolder holder = (ViewHolder) view.getTag();
+
+        boolean isMe = message.getAuthor().equals(mDisplayName);
+        setChatRowAppearance(isMe,holder);
+
+        String author = message.getAuthor();
+        holder.authorName.setText(author);
+
+        String msg = message.getMessage();
+        holder.body.setText(msg);
+
+        return view;
+    }
+
+    private void setChatRowAppearance(boolean isItMe, ViewHolder holder){
+        if (isItMe){
+            holder.params.gravity = Gravity.END;
+            holder.authorName.setTextColor(Color.GREEN);
+            holder.body.setBackgroundResource(R.drawable.bubble1);
+        }else {
+            holder.params.gravity = Gravity.START;
+            holder.authorName.setTextColor(Color.BLUE);
+            holder.body.setBackgroundResource(R.drawable.bubble2);
+        }
+    }
+
+    public void cleanup(){
+        mDatabaseReference.removeEventListener(mListener);
+    }
+}
